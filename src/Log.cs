f2bd0c199a2e00a424ec6f16570cd516707fa060
Retr0A <alexxretr0a@gmail.com>
2022-07-21 13:38:41 +0300

    Added Project Files + Log

---
+++ src/Log.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetroLog
+{
+    public class Log
+    {
+        private string m_Name;
+
+        public Log(string name)
+        {
+            m_Name = name;
+        }
+
+        public void PrintLogLine(object message, MessageType lineType)
+        {
+            if (lineType == MessageType.Info)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("[" + m_Name + "] " + message);
+            }
+            else if (lineType == MessageType.Success)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("[" + m_Name + "] " + message);
+            }
+            else if (lineType == MessageType.Warning)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("[" + m_Name + "] " + message);
+            }
+            else if (lineType == MessageType.Error)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("[" + m_Name + "] " + message);
+            }
+            else if (lineType == MessageType.Critical)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("[" + m_Name + "] " + message);
+            }
+        }
+    }
+}
