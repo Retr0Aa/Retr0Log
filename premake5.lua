732278d018959852183f3157fbfaa169ad491c68
Retr0A <alexxretr0a@gmail.com>
2022-08-09 13:30:53 +0300

    Added Premake Support

---
+++ premake5.lua
@@ -0,0 +1,14 @@
+project "Retr0Log"
+    kind "SharedLib"
+    language "C#"
+    targetdir "bin/${cfg.buildcfg}"
+
+    files { "**.cs" }
+
+    filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+
+    filter "configurations:Release"
+      defines { "RELEASE" }
+      optimize "On"
